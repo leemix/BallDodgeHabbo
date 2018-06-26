66279a6d90393ebb1043148bb34676824af23629
leemix <lekwc@live.com>
2018-06-26 15:59:39 -0300

    Add files via upload

---
+++ frmMain.Designer.cs
@@ -0,0 +1,275 @@
+﻿namespace BallDodge
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.checkBox3);
+            this.groupBox1.Controls.Add(this.label11);
+            this.groupBox1.Controls.Add(this.checkBox2);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label9);
+            this.groupBox1.Controls.Add(this.label10);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.checkBox1);
+            this.groupBox1.Location = new System.Drawing.Point(10, 5);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(199, 251);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Habbo Ball Dodge";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(15, 228);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(84, 13);
+            this.label11.TabIndex = 14;
+            this.label11.Text = "Coded by riko66";
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(123, 197);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(58, 17);
+            this.checkBox2.TabIndex = 13;
+            this.checkBox2.Text = "Debug";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.ForeColor = System.Drawing.Color.DarkRed;
+            this.label7.Location = new System.Drawing.Point(139, 104);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(13, 13);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "0";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(18, 158);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(53, 20);
+            this.textBox2.TabIndex = 8;
+            this.textBox2.Text = "1";
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(123, 158);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(53, 20);
+            this.textBox1.TabIndex = 7;
+            this.textBox1.Text = "2";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(125, 82);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(51, 13);
+            this.label8.TabIndex = 11;
+            this.label8.Text = "Target Y:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(15, 136);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(35, 13);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "Walk:";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.ForeColor = System.Drawing.Color.DarkRed;
+            this.label9.Location = new System.Drawing.Point(139, 56);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(13, 13);
+            this.label9.TabIndex = 10;
+            this.label9.Text = "0";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(125, 34);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(51, 13);
+            this.label10.TabIndex = 9;
+            this.label10.Text = "Target X:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(120, 134);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(49, 13);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "Distance";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.Color.DarkRed;
+            this.label3.Location = new System.Drawing.Point(29, 104);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(13, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "0";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(15, 82);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(49, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Player Y:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.DarkRed;
+            this.label2.Location = new System.Drawing.Point(29, 56);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(13, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "0";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Player X:";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(18, 197);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(59, 17);
+            this.checkBox1.TabIndex = 1;
+            this.checkBox1.Text = "Enable";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(123, 220);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(61, 17);
+            this.checkBox3.TabIndex = 15;
+            this.checkBox3.Text = "Hook 1";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.ClientSize = new System.Drawing.Size(216, 262);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmMain";
+            this.Opacity = 0.95D;
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Ball Dodge";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.CheckBox checkBox3;
+    }
+}
+
---
+++ frmMain.cs
@@ -0,0 +1,251 @@
+﻿using Sulakore.Habbo;
+using Sulakore.Modules;
+using System;
+using System.Windows.Forms;
+using Tangine;
+using Sulakore.Communication;
+using System.Reflection;
+using System.Net;
+using System.Runtime.InteropServices;
+
+namespace BallDodge
+{
+    [Module("BallDodge", "Allow you to automatically dodge the balls on habbo.")]
+    [Author("Riko66", HabboName = "Iterator", Hotel = HHotel.ComBr)]
+    public partial class frmMain : ExtensionForm
+    {
+        public override bool IsRemoteModule => true;
+        int iDistance = 2;
+        int iWalk = 1;
+
+        [DllImport("User32.dll")]
+        private static extern short GetAsyncKeyState(int vKey);
+
+        private static readonly int VK_F4 = 0x73; //This is the print-screen key.
+
+        //Assume the timer is setup with Interval = 16 (corresponds to ~60FPS).
+        private System.Windows.Forms.Timer timer1 = new System.Windows.Forms.Timer();
+
+        public frmMain()
+        {
+			InitializeComponent();
+			timer1.Interval = 80;
+			//timer1.Enabled = true;
+			timer1.Tick += new EventHandler(timer1_Tick);
+			KeyPreview = true;
+			KeyDown += new KeyEventHandler(Form_KeyDown);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            short keyState = GetAsyncKeyState(VK_F4);
+
+            bool f2 = ((keyState >> 15) & 0x0001) == 0x0001;
+            bool unprocessedPress = ((keyState >> 0) & 0x0001) == 0x0001;
+
+            if (f2)
+            {
+                checkBox1.Checked = !checkBox1.Checked;
+            }
+        }
+
+        private bool RemoteFileExists(string url)
+        {
+            try
+            {
+                HttpWebRequest request = WebRequest.Create(url) as HttpWebRequest;
+                request.Method = "HEAD";
+                HttpWebResponse response = request.GetResponse() as HttpWebResponse;
+                response.Close();
+                return (response.StatusCode == HttpStatusCode.OK);
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        private struct PlayerBase
+        {
+            public int x, y;
+
+            public PlayerBase(int posX, int posY)
+            {
+                x = posX;
+                y = posY;
+            }
+        }
+
+        int pPosX = 0;
+        int pPosY = 0;
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+        }
+
+        void Form_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.F3)
+            {
+                checkBox1.Checked = !checkBox1.Checked;
+            }
+        }
+
+        public void GetCurrentPos(DataInterceptedEventArgs e)
+        {
+            PlayerBase pPlayer;
+
+            e.Packet.ReadBytes(11);
+            int pY = (int)e.Packet.ReadBytes(1)[0];
+            e.Packet.ReadBytes(3);
+            int pX = (int)e.Packet.ReadBytes(1)[0];            
+
+            pPlayer.y = pY;
+            pPlayer.x = pX;
+
+            pPosX = pPlayer.x;
+            pPosY = pPlayer.y;
+
+            if (checkBox2.Checked)
+            {
+                label2.Text = pPosX.ToString();
+                label3.Text = pPosY.ToString();
+            }
+        }
+
+        public void SetCurrentPos(DataInterceptedEventArgs e)
+        {
+            PlayerBase pPlayer;
+            pPlayer.y = e.Packet.ReadInteger();
+            pPlayer.x = e.Packet.ReadInteger();
+            pPosX = pPlayer.x;
+            pPosY = pPlayer.y;
+
+            if (checkBox2.Checked)
+            {
+                label2.Text = pPosX.ToString();
+                label3.Text = pPosY.ToString();
+            }
+        }
+
+        private bool isItemNear(int fX, int fY, int iFactor)
+        {
+            if (fY + iFactor == pPosY || fY - iFactor == pPosY || fX + iFactor+1 == pPosX || fX - iFactor == pPosX)
+            {                
+                return true;
+            }
+
+            return true;
+        
+        }
+
+        private void DodgeBall(int fX, int fY, int iDistance_)
+        {
+            int iBallY = fY;
+            int iBallX = fX;
+            bool bUpdate = false;
+
+            if (isItemNear(iBallX, iBallY, 2) || isItemNear(iBallX, iBallY, 1))
+            {
+                if (pPosY - iDistance_ == iBallY || pPosY - 1 == iBallY)
+                {
+                    pPosY += iWalk;
+                    bUpdate = true;
+                }
+
+                if (pPosY + iDistance_ == iBallY || pPosY + 1 == iBallY)
+                {
+                    pPosY -= iWalk;
+                    bUpdate = true;
+                }
+
+                if (pPosX == iBallX + iDistance_ + 1 || pPosX == iBallX + 2)
+                {
+                    pPosX += iWalk;
+                    bUpdate = true;
+                }
+
+                if (pPosX + iDistance_ + 1 == iBallX || pPosX + 2 == iBallX)
+                {
+                    pPosX -= iWalk;
+                    bUpdate = true;
+                }
+
+                if (checkBox2.Checked)
+                {
+                    label2.Text = pPosX.ToString();
+                    label3.Text = pPosY.ToString();
+                }
+            }
+
+            if (bUpdate)
+                Connection.SendToServerAsync((ushort)676, pPosY, pPosX);
+        }
+
+        private void floorItemUpdate(DataInterceptedEventArgs e)
+        {
+            e.Packet.ReadBytes(7);
+            int furniX = (int)e.Packet.ReadBytes(1)[0];
+            e.Packet.ReadBytes(3);
+            int furniY = (int)e.Packet.ReadBytes(1)[0];
+
+            if (checkBox2.Checked)
+            {
+                label9.Text = furniX.ToString();
+                label7.Text = furniY.ToString();
+            }
+
+            DodgeBall(furniX, furniY, iDistance);
+        }
+
+        private void chkJsonOnly_CheckedChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+            {
+                if (checkBox3.Checked)
+                {
+                    Triggers.OutAttach((ushort)676, SetCurrentPos);
+                }
+                else
+                {
+                    Triggers.InAttach((ushort)1662, GetCurrentPos);
+                }
+
+                Triggers.InAttach((ushort)2978, floorItemUpdate);
+
+            }
+            else
+            {
+                if (checkBox3.Checked)
+                {
+                    Triggers.OutDetach((ushort)676);
+                }
+                else
+                {
+                    Triggers.InDetach((ushort)1662);
+                }
+
+                Triggers.InDetach((ushort)2978);
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            iDistance = Convert.ToInt32(textBox1.Text);
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            iWalk = Convert.ToInt32(textBox2.Text);
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
